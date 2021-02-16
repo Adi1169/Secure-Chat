39f06ff29858941f0b421099532f444cd5b3bded
Adi1169 <adityahfjc17@gmail.com>
2021-02-16 16:48:03 +0530

    Added Find User

--- app/src/main/java/com/example/securechat/MainPageActivity.java
+++ app/src/main/java/com/example/securechat/MainPageActivity.java
@@ -2,8 +2,12 @@ package com.example.securechat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,15 +15,25 @@ import com.example.securechat.MainActivity;
 import com.example.securechat.R;
 import com.google.firebase.auth.FirebaseAuth;
 
+import static android.Manifest.permission.READ_CONTACTS;
+
 public class MainPageActivity extends AppCompatActivity {
 
-    private Button mlogout;
+    private Button mlogout,mFindUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
 
         mlogout = findViewById(R.id.logout);
+        mFindUser = findViewById(R.id.findUser);
+
+        mFindUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), FindUserActivity.class));
+            }
+        });
 
         mlogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,5 +47,15 @@ public class MainPageActivity extends AppCompatActivity {
             }
         });
 
+
+        getPermissions();
+
+    }
+
+
+    private void getPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS},1);
+        }
     }
 }
